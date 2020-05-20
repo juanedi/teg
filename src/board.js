9efa08bcc068a412e8e3e618cfcbde50716f6951
Juan Edi <jedi11235@gmail.com>
2020-05-20 00:39:29 -0300

    attach pointer event handlers to countries

--- src/board.js
+++ src/board.js
@@ -1,8 +1,28 @@
 class TegBoard extends HTMLElement {
   connectedCallback() {
-    var embed = document.createElement("object")
+    let embed = document.createElement("object")
     embed.data = this.svgPath
     embed.type = "image/svg+xml"
+
+    embed.addEventListener("load", () => {
+      let svgDoc = embed.contentDocument
+
+      let linkElm = svgDoc.createElementNS("http://www.w3.org/1999/xhtml", "link");
+      linkElm.setAttribute("href", "/src/board.css");
+      linkElm.setAttribute("type", "text/css");
+      linkElm.setAttribute("rel", "stylesheet");
+      svgDoc.getElementsByTagNameNS("http://www.w3.org/2000/svg", "defs")[0].appendChild(linkElm);
+
+      let countries = Array.
+                        from(svgDoc.getElementById("paises").children).
+                        flatMap((c) => Array.from(c.children))
+
+      countries.forEach((c) => {
+        c.addEventListener("mouseover", (e) => e.target.classList.add("active-country"))
+        c.addEventListener("mouseout", (e) => e.target.classList.remove("active-country"))
+      })
+    })
+
     this.appendChild(embed)
   }
 }
