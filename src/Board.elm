cfef017127cec872c40764321f04bbd582755492
Juan Edi <jedi11235@gmail.com>
2020-05-21 02:01:45 -0300

    :art:

--- src/Board.elm
+++ src/Board.elm
@@ -94,42 +94,46 @@ attributes (Board guts) =
 
 styles : Board msg -> List Css.Global.Snippet
 styles (Board { highlightedCoutries }) =
-    List.map
-        (\country ->
-            id (Country.svgId country)
-                [ property "fill-opacity" "100% !important"
-                , property "stroke-width" "1px"
-                ]
-        )
-        highlightedCoutries
-        ++ [ id "inner_border"
+    List.concat
+        [ -- styles for highlighted countries
+          List.map
+            (\country ->
+                id (Country.svgId country)
+                    [ property "fill-opacity" "100% !important"
+                    , property "stroke-width" "1px"
+                    ]
+            )
+            highlightedCoutries
+        , -- static styles
+          [ id "inner_border"
                 [ fill (hex "#beddeb")
                 , property "stroke-dasharray" "3"
                 , property "stroke-opacity" "50%"
                 , property "stroke-width" "0.5px"
                 ]
-           , id "continents"
+          , id "continents"
                 [ Css.pointerEventsAll
                 ]
-           , selector "#continents *"
+          , selector "#continents *"
                 [ property "fill-opacity" "60%"
                 ]
-           , selector "#continents > #north_america * "
+          , selector "#continents > #north_america * "
                 [ fill (hex "#ca9782")
                 ]
-           , selector "#continents > #south_america *"
+          , selector "#continents > #south_america *"
                 [ fill (hex "#848585")
                 ]
-           , selector "#continents > #asia *"
+          , selector "#continents > #asia *"
                 [ fill (hex "#868485")
                 ]
-           , selector "#continents > #australia *"
+          , selector "#continents > #australia *"
                 [ fill (hex "#7badc7")
                 ]
-           , selector "#continents > #europe *"
+          , selector "#continents > #europe *"
                 [ fill (hex "#b2a7b4")
                 ]
-           , selector "#continents > #africa *"
+          , selector "#continents > #africa *"
                 [ fill (hex "#ab8c78")
                 ]
-           ]
+          ]
+        ]
