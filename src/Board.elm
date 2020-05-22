f13c154aae9d34bfcb7592acb1b9eab9f2ee8120
Juan Edi <jedi11235@gmail.com>
2020-05-21 22:19:56 -0300

    Fix: australia was not being highlighted

--- src/Board.elm
+++ src/Board.elm
@@ -27,7 +27,13 @@ view config =
         [ Html.Styled.fromUnstyled
             (Html.node "teg-board"
                 (Attr.property "svgPath" (Encode.string config.svgPath) :: attributes config)
-                [ Css.Global.global (styles config)
+                [ Css.Global.global
+                    -- (styles config)
+                    (List.concat
+                        [ staticStyles
+                        , List.concatMap highlightedCountryStyles config.highlightedCoutries
+                        ]
+                    )
                     |> -- NOTE: the stylesheet generation breaks when usinng a styled
                        -- node here.
                        Html.Styled.toUnstyled
@@ -61,48 +67,51 @@ attributes { onCountryClicked, onCountryMouseEnter, onCountryMouseLeave } =
         ]
 
 
-styles : { a | highlightedCoutries : List Country } -> List Css.Global.Snippet
-styles { highlightedCoutries } =
-    List.concat
-        [ -- styles for highlighted countries
-          List.map
-            (\country ->
-                id (Country.svgId country)
-                    [ property "fill-opacity" "100% !important"
-                    , property "stroke-width" "1px"
-                    ]
-            )
-            highlightedCoutries
-        , -- static styles
-          [ id "inner_border"
-                [ fill (hex "#beddeb")
-                , property "stroke-dasharray" "3"
-                , property "stroke-opacity" "50%"
-                , property "stroke-width" "0.5px"
-                ]
-          , id "continents"
-                [ Css.pointerEventsAll
-                ]
-          , selector "#continents *"
-                [ property "fill-opacity" "60%"
-                ]
-          , selector "#continents > #north_america * "
-                [ fill (hex "#ca9782")
-                ]
-          , selector "#continents > #south_america *"
-                [ fill (hex "#848585")
-                ]
-          , selector "#continents > #asia *"
-                [ fill (hex "#868485")
-                ]
-          , selector "#continents > #australia *"
-                [ fill (hex "#7badc7")
-                ]
-          , selector "#continents > #europe *"
-                [ fill (hex "#b2a7b4")
-                ]
-          , selector "#continents > #africa *"
-                [ fill (hex "#ab8c78")
-                ]
-          ]
+staticStyles : List Css.Global.Snippet
+staticStyles =
+    [ id "inner_border"
+        [ fill (hex "#beddeb")
+        , property "stroke-dasharray" "3"
+        , property "stroke-opacity" "50%"
+        , property "stroke-width" "0.5px"
         ]
+    , id "continents"
+        [ Css.pointerEventsAll
+        ]
+    , selector "#continents *"
+        [ property "fill-opacity" "60%"
+        ]
+    , selector "#continents > #north_america * "
+        [ fill (hex "#ca9782")
+        ]
+    , selector "#continents > #south_america *"
+        [ fill (hex "#848585")
+        ]
+    , selector "#continents > #asia *"
+        [ fill (hex "#868485")
+        ]
+    , selector "#continents > #australia *"
+        [ fill (hex "#7badc7")
+        ]
+    , selector "#continents > #europe *"
+        [ fill (hex "#b2a7b4")
+        ]
+    , selector "#continents > #africa *"
+        [ fill (hex "#ab8c78")
+        ]
+    ]
+
+
+highlightedCountryStyles : Country -> List Css.Global.Snippet
+highlightedCountryStyles country =
+    let
+        styles =
+            [ property "fill-opacity" "100% !important"
+            , property "stroke-width" "1px"
+            ]
+    in
+    [ -- matches paths and polygons
+      selector ("#" ++ Country.svgId country ++ ":empty") styles
+    , -- matches groups (needed for countries that consist of more than one element)
+      selector ("g#" ++ Country.svgId country ++ " *") styles
+    ]
