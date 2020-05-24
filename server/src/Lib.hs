8d68617a664bc218142415954abd067f9ab1c3ec
Juan Edi <jedi11235@gmail.com>
2020-05-23 23:21:41 -0300

    serve index.html at /

--- server/src/Lib.hs
+++ server/src/Lib.hs
@@ -21,6 +21,7 @@ data User = User
 $(deriveJSON defaultOptions ''User)
 
 type API = "users" :> Get '[JSON] [User]
+      :<|> Raw
 
 startApp :: IO ()
 startApp = run 8080 app
@@ -33,6 +34,7 @@ api = Proxy
 
 server :: Server API
 server = return users
+    :<|> serveDirectoryFileServer "."
 
 users :: [User]
 users = [ User 1 "Isaac" "Newton"
