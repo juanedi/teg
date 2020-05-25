eb1ccaa9aed7d483767b8378c818bb6a0cccc11e
Juan Edi <jedi11235@gmail.com>
2020-05-24 22:02:05 -0300

    Split Lib module into Server and Codegen

---
+++ src/Codegen.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeOperators #-}
+
+module Codegen (run) where
+
+import Elm.Derive (constructorTagModifier, defaultOptions, deriveBoth)
+import qualified Game
+import Servant.Elm (DefineElm (DefineElm), Proxy (Proxy), defElmImports, defElmOptions, generateElmModuleWith)
+import Server (APIRoutes)
+import System.Process (createProcess, cwd, proc)
+
+run :: IO ()
+run =
+  let outputDir = "./ui/generated"
+   in do
+        putStrLn "Deleting old generated code..."
+        putStrLn "Generating Elm code from API..."
+        generateElmModuleWith
+          defElmOptions
+          ["Api"]
+          defElmImports
+          outputDir
+          [ DefineElm (Proxy :: Proxy Game.State),
+            DefineElm (Proxy :: Proxy Game.Country)
+          ]
+          (Proxy :: Proxy APIRoutes)
+        putStrLn "Formatting generated code using elm-format..."
+        createProcess (proc "elm-format" ["Api.elm", "--yes"]) {cwd = Just outputDir}
+        putStrLn "Done!"
---
+++ src/Server.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeOperators #-}
+
+module Server
+  ( Server.run,
+    APIRoutes,
+  )
+where
+
+import Data.Maybe (fromMaybe)
+import Elm.Derive (constructorTagModifier, defaultOptions, deriveBoth)
+import qualified Game
+import Network.Wai
+import Network.Wai.Handler.Warp
+import Network.Wai.Logger (withStdoutLogger)
+import Servant
+import System.Environment (lookupEnv)
+
+type APIRoutes = "game" :> Get '[JSON] Game.State
+
+deriveBoth defaultOptions {constructorTagModifier = Game.tagToApiLabel} ''Game.Country
+deriveBoth defaultOptions ''Game.State
+
+type Routes =
+  APIRoutes
+    :<|> "_build" :> Raw
+    :<|> Raw
+
+run :: IO ()
+run = do
+  maybePort <- lookupEnv "PORT"
+  let port = fromMaybe 8080 (fmap read maybePort)
+  withStdoutLogger $ \logger -> do
+    let settings =
+          setPort port
+            $ setLogger logger
+            $ defaultSettings
+    putStrLn ("Starting the application at port " ++ show port)
+    runSettings settings app
+
+app :: Application
+app = serve api server
+
+api :: Proxy Routes
+api = Proxy
+
+server :: Server Routes
+server =
+  return Game.new
+    :<|> serveDirectoryWebApp "ui/_build"
+    :<|> serveDirectoryFileServer "ui/static"
