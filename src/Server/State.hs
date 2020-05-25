00d6e79c78442f3ea84dd8b923b33cfee0649c80
Juan Edi <jedi11235@gmail.com>
2020-05-25 15:17:41 -0300

    Initialize mutable server state using a TVar

---
+++ src/Server/State.hs
@@ -0,0 +1,25 @@
+module Server.State
+  ( State,
+    Server.State.init,
+    Server.State.read,
+    update,
+  )
+where
+
+import qualified Control.Concurrent.STM as STM
+import Control.Concurrent.STM.TVar (TVar)
+import qualified Control.Concurrent.STM.TVar as TVar
+import qualified Game
+
+type State = TVar Game.State
+
+init :: IO (State)
+init = TVar.newTVarIO (Game.init)
+
+read :: State -> IO (Game.State)
+read state =
+  TVar.readTVarIO state
+
+update :: (Game.State -> Game.State) -> State -> IO ()
+update fn state =
+  STM.atomically (TVar.modifyTVar state fn)
