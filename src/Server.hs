4da3fd9c4d9df0d944169d00ee83fb8d5b8528c7
Juan Edi <jedi11235@gmail.com>
2020-05-28 01:19:25 -0300

    wip: error handling

--- src/Server.hs
+++ src/Server.hs
@@ -8,7 +8,11 @@ module Server
   )
 where
 
+import qualified Control.Concurrent.STM as STM
+import qualified Control.Concurrent.STM.TVar as TVar
+import Control.Concurrent.STM.TVar (TVar)
 import Control.Monad.IO.Class (liftIO)
+import qualified Data.ByteString.Lazy
 import Data.Maybe (fromMaybe)
 import Data.Text (Text)
 import qualified Data.Text.Lazy
@@ -38,7 +42,7 @@ run :: IO ()
 run = do
   maybePort <- lookupEnv "PORT"
   let port = fromMaybe 8080 (fmap read maybePort)
-  serverState <- State.init
+  serverState <- TVar.newTVarIO State.init
   withStdoutLogger $ \logger -> do
     let settings =
           setPort port
@@ -47,13 +51,13 @@ run = do
     putStrLn ("Starting the application at port " ++ show port)
     runSettings settings (app serverState)
 
-app :: State -> Application
+app :: TVar State -> Application
 app serverState = serve api (server serverState)
 
 api :: Proxy Routes
 api = Proxy
 
-server :: State -> Server Routes
+server :: TVar State -> Server Routes
 server serverState =
   ( join serverState
       :<|> getState serverState
@@ -62,43 +66,63 @@ server serverState =
     :<|> serveDirectoryWebApp "ui/_build"
     :<|> serveDirectoryFileServer "ui/static"
 
-join :: State -> Handler Game.LocalState
-join serverState =
-  liftIO
-    ( State.update_
-        ( \gameState ->
-            let (player, updatedGameState) = Game.join gameState
-             in (Game.playerState player updatedGameState, updatedGameState)
+join :: TVar State -> Handler Game.LocalState
+join stateVar =
+  withState stateVar $ \state ->
+    case Game.join (State.gameState state) of
+      Left err ->
+        ( Left err,
+          state
         )
-        serverState
-    )
+      Right (localState, updatedGameState) ->
+        ( Right localState,
+          state {State.gameState = updatedGameState}
+        )
 
-getState :: State -> Text -> Handler Game.LocalState
-getState serverState playerId = do
+getState :: TVar State -> Text -> Handler Game.LocalState
+getState stateVar playerId = do
   player <- parsePlayerFromUrl playerId
-  liftIO
-    ( State.update_
-        ( \gameState ->
-            (Game.playerState player gameState, gameState)
+  withState stateVar $ \state ->
+    let gameState = State.gameState state
+     in ( Right (Game.playerState player (State.gameState state)),
+          state
         )
-        serverState
-    )
 
 parsePlayerFromUrl :: Text -> Handler Player
 parsePlayerFromUrl playerId =
   case parseUrlPiece playerId of
     Right player -> pure player
-    Left err -> throwError (err400 {errBody = encodeUtf8 (Data.Text.Lazy.fromStrict err)})
+    Left err -> throwError (err400 {errBody = encodeErrorMsg err})
 
-paintCountry :: State -> (Player, Country) -> Handler Game.LocalState
-paintCountry serverState (player, country) =
-  liftIO
-    ( State.update_
-        ( \gameState ->
-            let updatedGameState = Game.paintCountry player country gameState
-             in ( Game.playerState player updatedGameState,
-                  updatedGameState
-                )
-        )
-        serverState
-    )
+paintCountry :: TVar State -> (Player, Country) -> Handler Game.LocalState
+paintCountry stateVar (player, country) =
+  withState stateVar $ \state ->
+    let gameState = State.gameState state
+     in case Game.paintCountry player country gameState of
+          Left err -> (Left err, state)
+          Right updatedGameState ->
+            ( Right (Game.playerState player updatedGameState),
+              state {State.gameState = updatedGameState}
+            )
+
+handleError :: Game.Error -> Handler a
+handleError gameError =
+  case gameError of
+    Game.InvalidMove msg ->
+      throwError (err400 {errBody = encodeErrorMsg msg})
+    Game.InternalError msg ->
+      throwError (err500 {errBody = encodeErrorMsg msg})
+
+encodeErrorMsg :: Text -> Data.ByteString.Lazy.ByteString
+encodeErrorMsg msg =
+  encodeUtf8 (Data.Text.Lazy.fromStrict msg)
+
+withState ::
+  TVar State ->
+  (State -> (Either Game.Error a, State)) ->
+  Handler a
+withState stateVar fn = do
+  result <- liftIO (STM.atomically (STM.stateTVar stateVar fn))
+  case result of
+    Left err -> handleError err
+    Right value -> pure value
