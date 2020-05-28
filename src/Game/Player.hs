4df15602623f1ecc12206129c3d7707f08fbce72
Juan Edi <jedi@chappie.local>
2020-05-27 22:00:20 -0300

    Fix getState querying

--- src/Game/Player.hs
+++ src/Game/Player.hs
@@ -1,11 +1,15 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeOperators #-}
 
 module Game.Player (Player (..)) where
 
+import Data.Text (Text)
 import Elm.Derive (constructorTagModifier, defaultOptions, deriveBoth)
 import qualified Server.Serialization as Serialization
+import Web.HttpApiData (FromHttpApiData (..))
 
 data Player
   = Red
@@ -13,3 +17,13 @@ data Player
   deriving (Eq)
 
 deriveBoth defaultOptions {constructorTagModifier = Serialization.tagToApiLabel} ''Player
+
+instance FromHttpApiData Player where
+  parseUrlPiece :: Text -> Either Text Player
+  parseUrlPiece fragment =
+    if fragment == "red"
+      then Right Red
+      else
+        if fragment == "blue"
+          then Right Blue
+          else Left ("Could not interpret player from url fragment '" <> fragment <> "''")
