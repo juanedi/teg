583ffe364bf7e8fb6301cfee890e7338591d1edd
Juan Edi <jedi11235@gmail.com>
2020-06-01 06:41:26 -0300

    Detect start and end of a websocket connection

--- src/Server/WebSocket.hs
+++ src/Server/WebSocket.hs
@@ -9,7 +9,7 @@ where
 
 import Control.Concurrent.STM.TChan (TChan, readTChan)
 import Control.Monad.IO.Class (MonadIO)
-import Data.Conduit (ConduitT)
+import Data.Conduit (ConduitT, bracketP)
 import Data.Conduit (yield)
 import qualified Game
 import Game (Player)
@@ -22,22 +22,30 @@ type Routes =
   "socket" :> Capture "player" Player :> WebSocketConduit () Game.LocalState
 
 server :: State -> Server Routes
-server state = socketHandler
-  where
-    socketHandler :: MonadIO m => Player -> ConduitT () Game.LocalState m ()
-    socketHandler player = do
-      (currentGameState, playerChannel) <-
-        State.runSTM
-          ( do
-              chan <- State.playerUpdatesChannel state
-              gameState <- State.readGameState state
-              pure (gameState, chan)
-          )
-      yield (Game.playerState player currentGameState)
-      notificationLoop playerChannel player
+server state =
+  ( \player ->
+      bracketP
+        (playerConnected state player)
+        (playerDisconnected state player)
+        (startNotifications state player)
+  )
 
-notificationLoop :: MonadIO m => TChan Game.State -> Player -> ConduitT () Game.LocalState m ()
-notificationLoop playerChannel player = do
-  gameState <- State.runSTM (readTChan playerChannel)
-  yield $ Game.playerState player gameState
-  notificationLoop playerChannel player
+playerConnected :: State -> Player -> IO (TChan Game.State)
+playerConnected state player = do
+  putStrLn "------------------ player connected"
+  State.runSTM (State.playerUpdatesChannel state)
+
+startNotifications :: MonadIO m => State -> Player -> TChan Game.State -> ConduitT () Game.LocalState m ()
+startNotifications state player playerChannel = do
+  firstState <- State.runSTM (State.readGameState state)
+  notificationLoop player playerChannel firstState
+
+notificationLoop :: MonadIO m => Player -> TChan Game.State -> Game.State -> ConduitT () Game.LocalState m ()
+notificationLoop player playerChannel stateToNotify = do
+  yield $ Game.playerState player stateToNotify
+  newState <- State.runSTM (readTChan playerChannel)
+  notificationLoop player playerChannel newState
+
+playerDisconnected :: State -> Player -> TChan Game.State -> IO ()
+playerDisconnected state player playerChannel =
+  putStrLn "------------------ player disconnected"
