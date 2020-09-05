9f0be17d3cd625ee0b5b7add6616d584eb525bb5
Juan Edi <jedi11235@gmail.com>
2020-09-05 14:30:34 -0300

    Move color definitions to a module

---
+++ ui/src/Ui/Color.elm
@@ -0,0 +1,81 @@
+module Ui.Color exposing (..)
+
+import Css exposing (Color)
+
+
+
+-- COMMON UI THEME COLORS
+
+
+white : Color
+white =
+    Css.hex "#FFFFFF"
+
+
+grey : Color
+grey =
+    Css.hex "#CACACA"
+
+
+
+--  PLAYER COLORS
+
+
+blueSolid : Color
+blueSolid =
+    Css.rgba 0 0 255 1
+
+
+blueLight : Color
+blueLight =
+    Css.rgba 0 0 255 0.4
+
+
+redSolid : Color
+redSolid =
+    Css.rgba 255 0 0 1.0
+
+
+redLight : Color
+redLight =
+    Css.rgba 255 0 0 0.4
+
+
+blackSolid : Color
+blackSolid =
+    Css.rgba 0 0 0 1
+
+
+blackLight : Color
+blackLight =
+    Css.rgba 0 0 0 0.4
+
+
+yellowSolid : Color
+yellowSolid =
+    Css.rgba 255 234 0 1
+
+
+yellowLight : Color
+yellowLight =
+    Css.rgba 255 234 0 0.4
+
+
+greenSolid : Color
+greenSolid =
+    Css.rgba 0 255 0 1
+
+
+greenLight : Color
+greenLight =
+    Css.rgba 0 255 0 0.4
+
+
+magentaSolid : Color
+magentaSolid =
+    Css.rgba 255 0 255 1
+
+
+magentaLight : Color
+magentaLight =
+    Css.rgba 255 0 255 0.4
