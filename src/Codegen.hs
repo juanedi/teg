9e089e083c6e58df9193b41d1a5941a898bfe61f
Juan Edi <jedi@chappie.local>
2020-09-07 00:06:08 -0300

    Generate elm types

--- src/Codegen.hs
+++ src/Codegen.hs
@@ -7,32 +7,31 @@ module Codegen (run) where
 import qualified Client.ConnectionStates
 import qualified Client.Game
 import qualified Client.Room
+import Data.Proxy
+import Data.Text (pack)
+import qualified Data.Text.IO
+import Elm.Derive
+import Elm.Module
 import qualified Game
-import Servant.Elm (DefineElm (DefineElm), Proxy (Proxy), defElmImports, defElmOptions, generateElmModuleWith)
--- import Server (APIRoutes)
+import System.Directory (createDirectoryIfMissing)
+import System.FilePath.Posix (joinPath)
 import System.Process (createProcess, cwd, proc)
 
 run :: IO ()
 run =
-  return ()
-
--- let outputDir = "./ui/generated"
---  in do
---       putStrLn "Deleting old generated code..."
---       putStrLn "Generating Elm code from API..."
---       generateElmModuleWith
---         defElmOptions
---         ["Api"]
---         defElmImports
---         outputDir
---         [ DefineElm (Proxy :: Proxy Game.Country),
---           DefineElm (Proxy :: Proxy Game.Color),
---           DefineElm (Proxy :: Proxy Client.Game.Game),
---           DefineElm (Proxy :: Proxy Client.Game.Instructions),
---           DefineElm (Proxy :: Proxy Client.ConnectionStates.ConnectionStates),
---           DefineElm (Proxy :: Proxy Client.Room.Room)
---         ]
---         (Proxy :: Proxy APIRoutes)
---       putStrLn "Formatting generated code using elm-format..."
---       createProcess (proc "elm-format" ["Api.elm", "--yes"]) {cwd = Just outputDir}
---       putStrLn "Done!"
+  let outputDir = "./ui/generated"
+      fileName = "Api.elm"
+   in do
+        putStrLn "Deleting old generated code..."
+        putStrLn "Generating Elm code from API..."
+        createDirectoryIfMissing True outputDir
+        let content =
+              makeElmModule
+                "Api"
+                [ DefineElm (Proxy :: Proxy Game.Country),
+                  DefineElm (Proxy :: Proxy Game.Color)
+                ]
+        Data.Text.IO.writeFile (joinPath [outputDir, fileName]) (pack content)
+        putStrLn "Formatting generated code using elm-format..."
+        createProcess (proc "elm-format" [fileName, "--yes"]) {cwd = Just outputDir}
+        putStrLn "Done!"
