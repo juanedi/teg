1d08fd4a9ecf5232380046cdd2a178c86ddac362
Juan Edi <jedi11235@gmail.com>
2020-09-09 13:16:41 -0300

    Integrate Channel into the WebSocket module

--- src/Codegen.hs
+++ src/Codegen.hs
@@ -4,7 +4,6 @@
 
 module Codegen (run) where
 
-import qualified Channel
 import qualified Client.ConnectionStates
 import qualified Client.Game
 import qualified Client.Room
@@ -13,6 +12,7 @@ import Data.Text (pack)
 import qualified Data.Text.IO
 import Elm.Module
 import qualified Game
+import qualified Server.WebSocket as WebSocket
 import System.Directory (createDirectoryIfMissing)
 import System.FilePath.Posix (joinPath)
 import System.Process (createProcess, cwd, proc)
@@ -28,15 +28,15 @@ run =
         let content =
               makeElmModule
                 "Api"
-                [ DefineElm (Proxy :: Proxy Game.Country),
-                  DefineElm (Proxy :: Proxy Game.Color),
-                  DefineElm (Proxy :: Proxy Channel.ClientCommand),
-                  DefineElm (Proxy :: Proxy Channel.DataForClient),
-                  DefineElm (Proxy :: Proxy Client.ConnectionStates.ConnectionStates),
+                [ DefineElm (Proxy :: Proxy Client.ConnectionStates.ConnectionStates),
                   DefineElm (Proxy :: Proxy Client.Game.Game),
                   DefineElm (Proxy :: Proxy Client.Game.Instructions),
                   DefineElm (Proxy :: Proxy Client.Room.Lobby),
-                  DefineElm (Proxy :: Proxy Client.Room.Room)
+                  DefineElm (Proxy :: Proxy Client.Room.Room),
+                  DefineElm (Proxy :: Proxy Game.Country),
+                  DefineElm (Proxy :: Proxy Game.Color),
+                  DefineElm (Proxy :: Proxy WebSocket.ClientCommand),
+                  DefineElm (Proxy :: Proxy WebSocket.DataForClient)
                 ]
 
         Data.Text.IO.writeFile (joinPath [outputDir, fileName]) (pack content)
