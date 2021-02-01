69af9719848ba69dbdd0cc40f57fbb8cc2dc5ee7
Juan Edi <juan@noredink.com>
2021-02-01 20:33:39 -0300

    pull out ghc to a shared derivation
    to avoid repeating dependencies between shell and package build

--- shell.nix
+++ shell.nix
@@ -2,42 +2,7 @@ let
   sources = import ./nix/sources.nix;
   nixpkgs = import sources.nixpkgs { };
   niv = import sources.niv { };
-  haskellDeps = ps:
-    with ps; [
-      base
-      aeson
-      async
-      blaze-markup
-      blaze-html
-      bytestring
-      casing
-      conduit
-      containers
-      directory
-      elm-bridge
-      fast-logger
-      filepath
-      http-api-data
-      process
-      servant
-      servant-blaze
-      servant-server
-      servant-websockets
-      stm
-      text
-      uuid
-      wai
-      wai-app-static
-      wai-logger
-      warp
-      websockets
-
-      # for tests
-      hspec
-      hspec-wai
-      hspec-wai-json
-    ];
-  ghc = nixpkgs.haskellPackages.ghcWithPackages haskellDeps;
+  ghc = import ./nix/ghc.nix;
 in with nixpkgs;
 
 pkgs.mkShell {
